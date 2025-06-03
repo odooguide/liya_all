409c53ba479eb35d62bbfa9fc21ef8c2e64ad6c5
odooguide <enver.bodur@outlook.com>
2025-06-03 11:34:57 +0300

    Add files via upload

--- liya_sale_flow/models/crm_lead.py
+++ liya_sale_flow/models/crm_lead.py
@@ -1,7 +1,12 @@
 from odoo import models, fields
+from datetime import date
 
 class CrmLead(models.Model):
     _inherit = "crm.lead"
+
+    def _get_year_selection(self):
+        current_year = date.today().year
+        return [(str(y), str(y)) for y in range(current_year - 100, current_year + 11)]
 
     option1 = fields.Date(string="Option 1")
     option2 = fields.Date(string="Option 2")
@@ -12,7 +17,11 @@ class CrmLead(models.Model):
         ondelete="set null",
     )
     request_date = fields.Date(string="Request Date")
-    wedding_year = fields.Date(string="Wedding Year")
+    wedding_year = fields.Selection(
+        selection=_get_year_selection,
+        string="Düğün Yılı",
+        help="Sadece yıl seçilebilen alan",
+    )
     people = fields.Integer(string="People")
     second_contact = fields.Many2one(
         comodel_name="res.partner",
@@ -22,3 +31,5 @@ class CrmLead(models.Model):
     second_phone = fields.Char(string="Second Phone")
     second_mail = fields.Char(string="Second Mail")
     second_job_positino = fields.Char(string="Second Job Position")
+
+
