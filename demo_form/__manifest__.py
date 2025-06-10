3c5866c0071495a71185cbd05918180dd74ad296
Selçuk Atav <selcukatav@Selcuks-MacBook-Pro.local>
2025-06-10 12:20:14 +0200

    add demo form

---
+++ demo_form/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Liya Demo Form",
+    "version": "18.0.0.0",
+    "category": "Sale",
+    "depends": [ 'project'],
+    "author": "Broadmax",
+    'summary': 'Liya Demo Form',
+    "description": """
+        Liya Demo Form
+	""",
+    "website": "https://broadmax.com.tr",
+    "data": [
+        "views/demo_form.xml",
+        "views/project_template_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "auto_install": False,
+    "installable": True,
+}
