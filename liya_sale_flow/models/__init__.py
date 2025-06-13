363474b18e948373842c4708371be522176f8cd4
Selçuk Atav <selcukatav@Selcuks-MacBook-Pro.local>
2025-06-13 10:59:12 +0200

    update auto activity

--- liya_sale_flow/models/__init__.py
+++ liya_sale_flow/models/__init__.py
@@ -2,6 +2,5 @@ from . import crm_lead
 from . import wedding_type
 from . import sale_order_template
 from . import sale_order
-from . import project_template
 from . import calendar_alarm
 from . import activity_type
--- liya_sale_flow/models/activity_type.py
+++ liya_sale_flow/models/activity_type.py
@@ -4,3 +4,4 @@ class MailActivityType(models.Model):
     _inherit='mail.activity.type'
 
     is_quot=fields.Boolean(string='Teklif için mi?')
+    is_reminder=fields.Boolean(string='Teklif Hatirlatma için mi?')
