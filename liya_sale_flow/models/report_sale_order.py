a93e423968cfdb4dccb3ce91e8686390839131d8
alperenerS <alperen.er@steelify.io>
2025-06-19 23:23:28 +0300

    Fixed

--- liya_sale_flow/models/report_sale_order.py
+++ liya_sale_flow/models/report_sale_order.py
@@ -8,9 +8,11 @@ class ReportSaleorder(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None):
         orders = self.env['sale.order'].browse(docids)
-        # Eğer herhangi bir siparişte kişi sayısı 0 ise, raporu kes
+        # invalidse hata fırlat
         if any(order.people_count < 1 for order in orders):
             raise UserError(_(
                 "PDF raporu oluşturulamaz: 'Kişi Sayısı' 0 olamaz.\n"
                 "PDF report can't be created. People Count can't be 0 or below"
             ))
+        # geçerliyse, orijinal rapor değerlerini aynen döndür
+        return super()._get_report_values(docids, data=data)
