6f728eeb5a3afa2495e5411713786376b87b5ebb
alperenerS <alperen.er@steelify.io>
2025-06-19 13:41:36 +0300

    Fixed Pop-Up employee

--- liya_sale_flow/models/calendar_alarm.py
+++ liya_sale_flow/models/calendar_alarm.py
@@ -1,7 +1,23 @@
 from odoo import models, api,fields,_
 from odoo.exceptions import UserError
+
 class CalendarEvent(models.Model):
     _inherit = 'calendar.event'
+
+    @api.model
+    def _default_partners(self):
+        """ When active_model is res.partner, the current partners should be attendees """
+        partners = self.env.user.partner_id
+        active_id = self._context.get('active_id')
+        if self._context.get('active_model') == 'res.partner' and active_id and active_id not in partners.ids:
+                partners |= self.env['res.partner'].browse(active_id)
+        return partners
+    
+    partner_ids = fields.Many2many(
+        'res.partner', 'calendar_event_res_partner_rel',
+        string='Attendees', default=_default_partners,
+        domain=[('employee_ids', '!=', False)]
+        )
 
     @api.model
     def default_get(self, fields_list):
