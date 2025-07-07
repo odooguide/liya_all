5bad247194fb21eac6172572a52653137510bef3
Selçuk Atav <selcukatav4@gmail.com>
2025-07-07 14:33:12 +0200

    fix

--- liya_sale_flow/models/sale_order_event.py
+++ liya_sale_flow/models/sale_order_event.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api, _
+from datetime import datetime, timedelta
 
 
 class SaleOrderService(models.Model):
@@ -18,9 +19,37 @@ class SaleOrderProgram(models.Model):
     order_id = fields.Many2one('sale.order', string='Order')
     sale_order_template_id = fields.Many2one('sale.order.template', string='Sale Order Template')
     name = fields.Char(string='Event Name', )
-    start_datetime = fields.Datetime(string='Start Time', )
-    end_datetime = fields.Datetime(string='End Time', )
-    hours = fields.Char(string='Duration (hours)', store=True)
+    start_datetime = fields.Char(string='Start Time')
+    end_datetime = fields.Char(string='End Time' )
+    hours = fields.Char(
+        string='Duration (hours)',
+        compute='_compute_hours',
+        store=True,
+    )
+
+    @api.depends('start_datetime', 'end_datetime')
+    def _compute_hours(self):
+        fmt = '%H:%M'
+        for rec in self:
+            sd = rec.start_datetime
+            ed = rec.end_datetime
+            if sd and ed:
+                try:
+                    t1 = datetime.strptime(sd, fmt)
+                    t2 = datetime.strptime(ed, fmt)
+                except ValueError:
+                    rec.hours = False
+                    continue
+
+                if t2 < t1:
+                    t2 += timedelta(days=1)
+
+                diff = t2 - t1
+                h = diff.seconds // 3600
+                m = (diff.seconds % 3600) // 60
+                rec.hours = '{}:{:02d}'.format(h, m)
+            else:
+                rec.hours = False
 
 
 class SaleOrderTransport(models.Model):
@@ -31,4 +60,4 @@ class SaleOrderTransport(models.Model):
     sale_order_template_id = fields.Many2one('sale.order.template', string='Sale Order Template')
     departure_location = fields.Char(string='Departure Location')
     arrival_location = fields.Char(string='Arrival Location')
-    arrival_datetime = fields.Datetime(string='Departure Time')
+    arrival_datetime = fields.Char(string='Departure Time')
