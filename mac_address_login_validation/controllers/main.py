3f4993b9613dfba8d02e68408a060435c3a9b02a
Selçuk Atav <selcukatav4@gmail.com>
2025-07-18 23:46:20 +0200

    fix

--- mac_address_login_validation/controllers/main.py
+++ mac_address_login_validation/controllers/main.py
@@ -1,30 +1,9 @@
-# -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2022-November Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
+
 import logging
 import sys
 import subprocess
 from odoo.addons.web.controllers import home
 from odoo.http import request
-from odoo.exceptions import Warning
 import odoo
 import odoo.modules.registry
 from odoo.tools.translate import _
