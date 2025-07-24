93ae825ca6442d7949a6ab99b96ea4280cd76179
Selçuk Atav <selcukatav4@gmail.com>
2025-07-25 01:35:01 +0300

    add compute on day

--- demo_form/models/demo_form.py
+++ demo_form/models/demo_form.py
@@ -13,7 +13,7 @@ class ProjectDemoForm(models.Model):
         default=lambda self: _('New Demo Form'))
     invitation_owner = fields.Char(string="Invitation Owner")
     invitation_date = fields.Date(string="Invitation Date")
-    duration_days = fields.Char(string="Day")
+    duration_days = fields.Char(string="Day",compute='_compute_day')
     demo_date = fields.Date(string="Demo Date")
     special_notes = fields.Text(string="Special Notes")
 
@@ -323,7 +323,7 @@ class ProjectDemoForm(models.Model):
         return super().create(vals)
 
     @api.depends('invitation_date')
-    def _compute_activity_day(self):
+    def _compute_day(self):
         turkish_days = [
             'Pazartesi', 'Salı', 'Çarşamba',
             'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'
