2c3a4bbdb09be5f736edf3845633a8e2068e2a4e
Selçuk Atav <selcukatav4@gmail.com>
2025-07-30 15:32:26 +0300

    add tag ids and requirements for crm

--- liya_sale_flow/models/sale_order_line.py
+++ liya_sale_flow/models/sale_order_line.py
@@ -3,6 +3,9 @@ from odoo.exceptions import UserError
 
 class SaleOrderLine(models.Model):
     _inherit='sale.order.line'
+
+    tag_ids=fields.Many2many(related='order_id.tag_ids',string='Tag Ids')
+
     @api.onchange('product_template_id')
     def _onchange_order_line_template(self):
         if not self.order_id.sale_order_template_id:
